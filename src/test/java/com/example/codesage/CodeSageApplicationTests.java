2b355196c2e033d14286fe7bd960929ba0a0e657
VavilovNikita <vavilovnik00@mail.ru>
2025-06-12 18:25:33 +0300

    Initial commit

---
+++ src/test/java/com/example/codesage/CodeSageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codesage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
